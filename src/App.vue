21d2e97016e58344114b675bf466c9f1b8d933af
patricklongo1 <ck.longo@bol.com.br>
2020-02-22 20:41:54 -0300

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <div id="app">
-    <h3>Opa!</h3>
-  </div>
+  <v-app id="app">
+      <v-app-bar
+      absolute
+      color="deep-purple accent-4"
+      dark
+      scroll-target="#scrolling-techniques-6"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Collapsing Bar</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-checkbox
+        v-model="collapseOnScroll"
+        color="white"
+        hide-details
+      ></v-checkbox>
+    </v-app-bar>
+  </v-app>
 </template>
